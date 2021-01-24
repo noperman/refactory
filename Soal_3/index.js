2df9aa55e3835c25144b0d9268ca02a71682b4dd
noperman <kenablocked@gmail.com>
2021-01-24 08:52:37 +0700

    soal_3

---
+++ Soal_3/index.js
@@ -0,0 +1,86 @@
+const data = require("./data.json")
+
+// 1 Find items in Meeting Room.
+let quest1 = []
+search(
+  data,
+  "if(element.placement.name.toLowerCase() == 'meeting room'){"+
+    "quest1.push(element)"+
+  "}"
+)
+// 2 Find all electronic devices.
+let quest2 = []
+search(
+  data,  
+  "if(element.type.toLowerCase() == 'electronic'){"+
+    "quest2.push(element)"+
+  "}"
+)
+// 3 Find all furnitures.
+let quest3 = []
+search(
+  data,  
+  "if(element.type.toLowerCase() == 'furniture'){"+
+    "quest3.push(element)"+
+  "}"
+)
+// 4 Find all items was purchased at 16 Januari 2020.
+let quest4 = []
+search(
+  data,  
+  "console.log(new Date(element.purchased_at))"
+  // "if(element.type.toLowerCase() == 'furniture'){"+
+  //   "quest4.push(element)"+
+  // "}"
+)
+// 5 Find all items with brown color.
+let quest5 = []
+search(
+  data,  
+  "if(element.tags.length > 0){"+
+    "element.tags.forEach(subel => {"+
+      "if(subel.toLowerCase() == 'brown'){"+
+        "quest5.push(element)"+
+      "}"+
+    "})"+
+  "}"
+)
+
+console.log(
+  // 1 Find items in Meeting Room.
+  "1. Find items in Meeting Room. : ",
+  spitter(), 
+  quest1,
+  spitter(),
+  // 2 Find all electronic devices.
+  "2. Find all electronic devices. : ",
+  spitter(), 
+  quest2,
+  spitter(),
+  // 3 Find all furnitures.
+  "3. Find all furnitures. : ",
+  spitter(), 
+  quest3,
+  spitter(),
+  // 4 Find all items was purchased at 16 Januari 2020.
+  "4. Find all items was purchased at 16 Januari 2020. : ",
+  spitter(), 
+  // quest4,
+  spitter(),
+  // 5 Find all items with brown color.
+  "5. Find all items with brown color. : ",
+  spitter(), 
+  quest5,
+  spitter()
+)
+
+function search(data,condition){
+  data.forEach(element => {
+    eval(condition)
+  });
+}
+
+
+function spitter(){
+  return "\n ======================================================= \n"
+}
